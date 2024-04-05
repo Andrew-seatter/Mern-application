33bc68f3addda775bb403eb73262637a8fa40ec1
Andrew Seatter <135391186+Andrew-seatter@users.noreply.github.com>
2024-04-05 00:18:08 -0700

    deployment fix

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -22,7 +22,7 @@ const SignupForm = () => {
   const handleFormSubmit = async (event) => {
     event.preventDefault();
 
-    // check if form has everything (as per react-bootstrap docs)
+    
     const form = event.currentTarget;
     if (form.checkValidity() === false) {
       event.preventDefault();
