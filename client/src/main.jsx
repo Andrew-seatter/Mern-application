9722ef8828eca8b70030d9856197caac2f882fdc
Andrew Seatter <135391186+Andrew-seatter@users.noreply.github.com>
2024-04-04 22:36:56 -0700

    server test sucessful

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,33 @@
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import { setContext } from '@apollo/client/link/context';
 import App from './App.jsx'
 import SearchBooks from './pages/SearchBooks'
 import SavedBooks from './pages/SavedBooks'
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+// Create a middleware for the http link that will attach the JWT as an Authorization header
+const authLink = setContext((_, { headers }) => {
+  // get the authentication token from local storage if it exists
+  const token = localStorage.getItem('token');
+  // return the headers to the context so httpLink can read them
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink), // Chain it with the httpLink
+  cache: new InMemoryCache(),
+});
 
 const router = createBrowserRouter([
   {
@@ -24,5 +47,9 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+  <ApolloProvider client={client}>
+      <RouterProvider>
+        <App />
+      </RouterProvider>
+  </ApolloProvider>
 )
